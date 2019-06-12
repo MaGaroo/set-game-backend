aa087314f9105351e489a3705f1e18912d7a9b53
MaGaroo <amoomajid99@gmail.com>
2019-06-13 04:11:35 +0430

    Save config in service

--- src/service/Service.go
+++ src/service/Service.go
@@ -3,12 +3,14 @@ package service
 import "github.com/jinzhu/gorm"
 
 type Service struct {
-	DB *gorm.DB
+	DB     *gorm.DB
+	config Config
 }
 
 func NewService(cfg *Config) (service *Service, err error) {
 	if service.DB, err = gorm.Open("sqlite3", cfg.DBAddress); err != nil {
 		return nil, err
 	}
+	service.config = *cfg
 	return service, nil
 }
