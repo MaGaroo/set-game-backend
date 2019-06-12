62ae65e8f3023ec0efc46b4bc068f82aeaa9231a
MaGaroo <amoomajid99@gmail.com>
2019-06-13 04:09:38 +0430

    Add new service method

--- src/service/Service.go
+++ src/service/Service.go
@@ -5,3 +5,10 @@ import "github.com/jinzhu/gorm"
 type Service struct {
 	DB *gorm.DB
 }
+
+func NewService(cfg *Config) (service *Service, err error) {
+	if service.DB, err = gorm.Open("sqlite3", cfg.DBAddress); err != nil {
+		return nil, err
+	}
+	return service, nil
+}
