e816c0bf169b1c7b332c8efc4f85710c1dca9e2c
MaGaroo <amoomajid99@gmail.com>
2019-06-14 19:20:22 +0430

    Implement register method

--- src/service/handlers.go
+++ src/service/handlers.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"github.com/gorilla/websocket"
 	"fmt"
+	"set-game/src/models"
+	"set-game/src/service/messages"
 )
 
 func (service *Service) setupRoutes() {
@@ -20,8 +22,46 @@ func (service *Service) wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err.Error())
 	}
+	defer ws.Close()
 
-	service.reader(ws)
+	player := service.register(ws)
+	if player != nil {
+		service.play(player)
+	}
+}
+
+func (service *Service) register(conn *websocket.Conn) *models.Player {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return nil
+		}
+		if messageType != 1 {
+			continue
+		}
+
+		req := messages.ParseIntroRequest(string(p))
+		if req == nil {
+			continue
+		}
+
+		room, err := service.getRoom(req.RoomToken)
+		if err != nil {
+			return nil
+		}
+		player, err := service.getOrCreatePlayer(req.PlayerToken, req.Username, room)
+		if err != nil {
+			return nil
+		}
+		return player
+	}
+	return nil
+}
+
+func (service *Service) play(player *models.Player) {
+
 }
 
 func (service *Service) reader(conn *websocket.Conn) {
