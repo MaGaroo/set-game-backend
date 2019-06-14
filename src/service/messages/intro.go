c437b174c62faf419f6f52d7ffa39c9533f06d6b
MaGaroo <amoomajid99@gmail.com>
2019-06-14 19:20:51 +0430

    Parse intro requests

--- src/service/messages/intro.go
+++ src/service/messages/intro.go
@@ -1,11 +1,30 @@
 package messages
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type IntroRequest struct {
-	GameToken   string
+	RoomToken   string
 	Username    string
 	PlayerToken string
+}
+
+func ParseIntroRequest(message string) *IntroRequest {
+	args := strings.Fields(message)
+	if args[0] != "intro" || (len(args) != 4 && len(args) != 3) {
+		return nil
+	}
+	token := ""
+	if len(args) == 4 {
+		token = args[3]
+	}
+	return &IntroRequest{
+		RoomToken:   args[1],
+		Username:    args[2],
+		PlayerToken: token,
+	}
 }
 
 type IntroResponse struct {
